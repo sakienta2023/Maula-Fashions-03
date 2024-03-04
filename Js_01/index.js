406e3f57796d39e074657dcc0719f529ce494ff6
sakienta2023 <127915543+sakienta2023@users.noreply.github.com>
2024-03-04 15:05:31 +0000

    Updated stylesheet  Js Structure

--- Js_01/index.js
+++ Js_01/index.js
@@ -6,3 +6,23 @@ if(hamburger){
         navlist.classList.toggle("open");
     });
 }
+
+//Popup
+const popup = document.querySelector('.popup');
+
+const closePopup = document.querySelector('.close-popup');
+
+if(popup){
+  closePopup.addEventListener( "click", ()=> {
+    popup.classList.add('hide-popup');
+  });
+
+  window.addEventListener('load',()=> {
+    setTimeout(() => {
+      popup.classList.remove('hide-popup');
+    }, 1000);
+  })
+}
+
+
+  
